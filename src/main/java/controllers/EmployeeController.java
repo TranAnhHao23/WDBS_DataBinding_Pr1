a9dc2afdb376d183b48b2c73a14b174cab9cb57c
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-02-22 10:48:06 +0700

    practise 01

---
+++ src/main/java/controllers/EmployeeController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping
+    public ModelAndView showForm(){
+        ModelAndView modelAndView = new ModelAndView("view");
+        modelAndView.addObject("employee",new Employee());
+        return modelAndView;
+    }
+
+    @GetMapping("create")
+    public ModelAndView create(){
+        ModelAndView modelAndView = new ModelAndView("create");
+        Employee employee = new Employee();
+        modelAndView.addObject("employee",employee);
+        return modelAndView;
+    }
+
+    @PostMapping
+    public ModelAndView addEmployee(@ModelAttribute Employee employee){
+        ModelAndView modelAndView = new ModelAndView("view");
+        showForm();
+        return modelAndView;
+    }
+}
